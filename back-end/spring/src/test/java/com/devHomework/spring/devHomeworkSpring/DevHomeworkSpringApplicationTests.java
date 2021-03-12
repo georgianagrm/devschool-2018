492ae9767e9a953b6789a073b2d767425af58ebf
georgianagrm <44406844+georgianagrm@users.noreply.github.com>
2021-03-12 16:59:52 +0200

    add-spring

---
+++ back-end/spring/src/test/java/com/devHomework/spring/devHomeworkSpring/DevHomeworkSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devHomework.spring.devHomeworkSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevHomeworkSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
